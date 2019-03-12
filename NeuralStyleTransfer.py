c0aca8f8dac49dec7230e4997d745bd736bb1584
Houssam Mehdi <houssam_mahdi_98@hotmail.com>
2019-03-12 10:04:46 +0100

    Update NeuralStyleTransfer.py
    Imagesize to 512 instead of 1080 (GPU computation time)
    Defined gram_matrix

--- NeuralStyleTransfer.py
+++ NeuralStyleTransfer.py
@@ -15,15 +15,15 @@ import copy
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(torch.cuda.is_available())
 
-imsize = 1080 if torch.cuda.is_available() else 128 
+imsize = 512 if torch.cuda.is_available() else 128 
 
 loader = transforms.Compose([
     transforms.Resize(imsize), 
     transforms.ToTensor()])
 
-    unloader = transforms.ToPILImage()
-
-    def imshow(tensor, title=None):
+unloader = transforms.ToPILImage()
+   
+def imshow(tensor, title=None):
     image = tensor.cpu().clone()  
     image = image.squeeze(0)   
     image = unloader(image)
@@ -33,7 +33,7 @@ loader = transforms.Compose([
     if title is not None:
         plt.title(title)
 
-
+        
 class ContentLoss(nn.Module):
 
     def __init__(self, target,):
@@ -43,3 +43,12 @@ class ContentLoss(nn.Module):
     def forward(self, input):
         self.loss = F.mse_loss(input, self.target)
         return input
+
+def gram_matrix(input):
+    a, b, c, d = input.size()  
+    # a=batch size
+    # b=number of feature maps
+    # c,d=dimensions of a f
+    features = input.view(a * b, c * d)
+    G = torch.mm(features, features.t())  
+    return G.div(a * b * c * d)

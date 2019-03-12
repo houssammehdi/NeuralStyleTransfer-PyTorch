8fc1ab446635a77e893c3ccb2475444dc94cc3fd
Houssam Mehdi <houssam_mahdi_98@hotmail.com>
2019-03-12 10:28:01 +0100

    Update NeuralStyleTransfer.py
    comments

--- NeuralStyleTransfer.py
+++ NeuralStyleTransfer.py
@@ -20,15 +20,18 @@ imsize = 512 if torch.cuda.is_available() else 128
 loader = transforms.Compose([
     transforms.Resize(imsize), 
     transforms.ToTensor()])
+# resizing and converting image to tensor
 
 unloader = transforms.ToPILImage()
+# reconverting from tensor to image
    
 def imshow(tensor, title=None):
-    image = tensor.cpu().clone()  
-    image = image.squeeze(0)   
-    image = unloader(image)
+    image = tensor.cpu().clone()  # clone tensor 
+    image = image.squeeze(0)  # remove first dimension
+    image = unloader(image) # reconvert to image
     image.save('output.jpg')
     files.download('output.jpg')
+    # download image (used for colab)
     plt.imshow(image)
     if title is not None:
         plt.title(title)
@@ -37,11 +40,16 @@ def imshow(tensor, title=None):
 class ContentLoss(nn.Module):
 
     def __init__(self, target,):
+        # constructor takes feature maps as input -> returns the weighted content distance
         super(ContentLoss, self).__init__()
         self.target = target.detach()
+        # dynamically computing the gradients
 
     def forward(self, input):
+        # transparent content loss layer
         self.loss = F.mse_loss(input, self.target)
+        # ∥FXL−FCL∥ ^ 2
+        # loss saved as a parameter of the module
         return input
 
 def gram_matrix(input):
@@ -50,5 +58,12 @@ def gram_matrix(input):
     # b=number of feature maps
     # c,d=dimensions of a feature map
     features = input.view(a * b, c * d)
+    # reshaping FXL into ^FXL -> KxN matrix 
+    # K = number of feature maps at layer L
+    # N = lenght of vectorized feature map
     G = torch.mm(features, features.t())  
+    # multiplying matrix by transpose
+
     return G.div(a * b * c * d)
+    # normalizing by dividing by the number of elements in each f. map
+    # crucial step because larger values have larger impact during gradient descent

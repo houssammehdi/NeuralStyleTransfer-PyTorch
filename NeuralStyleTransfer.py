3b70bcaabf0e5218a758affb49480b7c53323dd8
Houssam Mehdi <houssam_mahdi_98@hotmail.com>
2019-03-24 21:13:42 +0100

    Update NeuralStyleTransfer.py
    Added style and content loss computation
    Added optimizer and input img

--- NeuralStyleTransfer.py
+++ NeuralStyleTransfer.py
@@ -13,18 +13,28 @@ from google.colab import files
 import copy
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# Checking if cuda (gpu) is available
 print(torch.cuda.is_available())
 
 imsize = 512 if torch.cuda.is_available() else 128 
+# using bigger image size if GPU available, if not we use a small size for the CPU
 
+# Loading the style and content images
+style_img = image_loader("images/1920x1200.jpg")
+content_img = image_loader("images/nature.jpg")
+
+
+# Resizing and converting image to tensor
 loader = transforms.Compose([
     transforms.Resize(imsize), 
     transforms.ToTensor()])
-# resizing and converting image to tensor
 
+
+# Reconverting from tensor to image
 unloader = transforms.ToPILImage()
-# reconverting from tensor to image
-   
+
+ 
+# Displays an image by reconverting a copy of it to PIL 
 def imshow(tensor, title=None):
     image = tensor.cpu().clone()  # clone tensor 
     image = image.squeeze(0)  # remove first dimension
@@ -37,6 +47,7 @@ def imshow(tensor, title=None):
         plt.title(title)
 
         
+# Content Loss Module
 class ContentLoss(nn.Module):
 
     def __init__(self, target,):
@@ -52,6 +63,8 @@ class ContentLoss(nn.Module):
         # loss saved as a parameter of the module
         return input
 
+    
+# Gram Matrix computation
 def gram_matrix(input):
     a, b, c, d = input.size()  
     # a=batch size
@@ -68,6 +81,8 @@ def gram_matrix(input):
     # normalizing by dividing by the number of elements in each f. map
     # crucial step because larger values have larger impact during gradient descent
     
+    
+# Style Loss Module
 class StyleLoss(nn.Module):
 
     def __init__(self, target_feature):
@@ -82,14 +97,14 @@ class StyleLoss(nn.Module):
         return input
     
     
+# importing pre-trained model, evaluation mode   
+# normalizing the images before sending them to the network
 cnn = models.vgg19(pretrained=True).features.to(device).eval()
-# importing pre-trained model, evaluation mode
 cnn_normalization_mean = torch.tensor([0.485, 0.456, 0.406]).to(device)
 cnn_normalization_std = torch.tensor([0.229, 0.224, 0.225]).to(device)
-# normalizing the images before sending them to the network
 
-# create a module to normalize input image so we can easily put it in a
-# nn.Sequential
+
+# Creating a module to normalize input image so we can easily put it in a nn.Sequential
 class Normalization(nn.Module):
     def __init__(self, mean, std):
         super(Normalization, self).__init__()
@@ -103,15 +118,67 @@ class Normalization(nn.Module):
         # normalize img
         return (img - self.mean) / self.std
     
+    
 # desired depth layers to compute style and content losses
-content_layers = ['conv_4']
-style_layers = ['conv_1', 'conv_2', 'conv_3', 'conv_4', 'conv_5']
 # the layers can be changed but we will use these layers as
 # they have been proven to give great results for the topic
+content_layers = ['conv_4']
+style_layers = ['conv_1', 'conv_2', 'conv_3', 'conv_4', 'conv_5']
 
+
+# Computing the style and content losses
 def get_style_and_losses(style_img, content_img):
-    
+
     normalization = Normalization(cnn_normalization_mean, cnn_normalization_std).to(device)
+    # Getting the Normalization module
     content_losses = []
     style_losses = []
+    # Iterable access to content and style losses
     model = nn.Sequential(normalization)
+    # New nn.Sequential -> Normalization sequencial activation
+
+    i = 0  # increment every time we see a conv
+    for layer in cnn.children():
+        if isinstance(layer, nn.Conv2d):
+            i += 1
+            name = 'conv_{}'.format(i)
+            # Looping over Conv2d layer and renaming to conv_(layer number)
+        
+        model.add_module(name, layer)
+
+        if name in content_layers:
+            # add content loss:
+            target = model(content_img).detach()
+            content_loss = ContentLoss(target)
+            model.add_module("content_loss_{}".format(i), content_loss)
+            content_losses.append(content_loss)
+
+        if name in style_layers:
+            # add style loss:
+            target_feature = model(style_img).detach()
+            style_loss = StyleLoss(target_feature)
+            model.add_module("style_loss_{}".format(i), style_loss)
+            style_losses.append(style_loss)
+
+    # Removing the layers after the last content and style losses
+    for i in range(len(model) - 1, -1, -1):
+        if isinstance(model[i], ContentLoss) or isinstance(model[i], StyleLoss):
+            break
+
+    model = model[:(i + 1)] # Everything until i + 1 -> 0 to i
+
+    return model, style_losses, content_losses
+
+
+# Using a noise image as an input image which is generated from the content image
+input_img = torch.randn(content_img.data.size(), device=device)
+
+
+# Optimzer L-BFGS
+def get_input_optimizer(input_img):
+    # We will use L-BFGS algorithm to run our gradient descent
+    optimizer = optim.LBFGS([input_img.requires_grad_()])
+    # Creating a PyTorch L-BFGS optimizer and we pass the image as the tensor to optimize 
+    return optimizer
+
+
